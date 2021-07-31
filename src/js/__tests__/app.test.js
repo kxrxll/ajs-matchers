f4458c0464f38fb4153e41aaf24fbeb87e011e72
kxrxll <kovalyovkirill@gmail.com>
2021-07-31 11:30:26 +0300

    solved

---
+++ src/js/__tests__/app.test.js
@@ -0,0 +1,27 @@
+import sortHeroesByHealth from '../app';
+
+test('should sort', () => {
+  const result = sortHeroesByHealth([
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ]);
+  expect(result).toEqual([
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ]);
+});
+
+test('should sort same', () => {
+  const result = sortHeroesByHealth([
+    { name: 'мечник', health: 100 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ]);
+  expect(result).toEqual([
+    { name: 'мечник', health: 100 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ]);
+});
--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,11 @@
-// TODO: write your code here
-import sum from './basic';
-
-console.log('worked');
-
-console.log(sum([1, 2]));
+export default function sortHeroesByHealth(arr) {
+  return arr.sort((a, b) => {
+    if (a.health > b.health) {
+      return -1;
+    }
+    if (a.health < b.health) {
+      return 1;
+    }
+    return 0;
+  });
+}
