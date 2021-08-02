e8d6ba96d10cd34f4885b4bf2aa00fc93030b346
kxrxll <kovalyovkirill@gmail.com>
2021-08-02 14:42:38 +0300

    files deleted and toBe checking added

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -13,6 +13,19 @@ test('should sort', () => {
   ]);
 });
 
+test('should sort', () => {
+  const result = sortHeroesByHealth([
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ]);
+  expect(result).not.toBe([
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ]);
+});
+
 test('should sort same', () => {
   const result = sortHeroesByHealth([
     { name: 'мечник', health: 100 },
